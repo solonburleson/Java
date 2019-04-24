99da6aa292ba27bee438a7cec16247eadfda5521
solonburleson <solon.burleson@gmail.com>
2019-04-24 14:05:06 -0500

    ZooKeeper

---
+++ ZooKeeper/Gorilla.java
@@ -0,0 +1,16 @@
+public class Gorilla extends Mammal{
+    public void throwSomething(){
+        System.out.println("Throwing Something");
+        this.energy -= 5;        
+    }
+
+    public void eatBananas(){
+        System.out.println("Eating Bananas");
+        this.energy += 10;
+    }
+    
+    public void climb(){
+        System.out.println("Climbing a tree");
+        this.energy -= 10;    
+    }
+}
---
+++ ZooKeeper/MammalTest.java
@@ -0,0 +1,17 @@
+public class MammalTest{
+    public static void main(String args[]){
+        Gorilla joe = new Gorilla();
+        joe.throwSomething();
+        joe.showEnergy();
+        joe.throwSomething();
+        joe.showEnergy();
+        joe.throwSomething();
+        joe.showEnergy();
+        joe.eatBananas();
+        joe.showEnergy();
+        joe.eatBananas();
+        joe.showEnergy();
+        joe.climb();
+        joe.showEnergy();
+    }
+}
